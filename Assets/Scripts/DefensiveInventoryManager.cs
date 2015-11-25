e3be19b514fac21dcccba7e19304ffefb89e5af1
Eli Whitbread <eli@whitbreadgames.com>
2015-11-25 19:42:38 +1100

    Added basic Tower Spawn system with "look at path" to TowerPlacementTest scene. Fixed test prefabs for all units. Player can still move turrets once placed.

---
+++ Assets/Scripts/DefensiveInventoryManager.cs
@@ -0,0 +1,95 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class DefensiveInventoryManager : MonoBehaviour {
+
+	public GameObject Unit1, Unit2, Unit3;
+	public int maxUnit1Number, maxUnit2Number, maxUnit3Number;
+	public int curAvailableUnit1s, curAvailableUnit2s, curAvailableUnit3s;
+	public float positioningLookDist = 3.0f;
+	public GameObject lookTestSphere;
+	public Vector3 lookAtPathPoint;
+	
+	GameObject curSpawnedUnit;
+	RaycastHit hit;
+	float unitPosX;
+	float unitPosZ;
+	Vector3 unitFollowPos;
+
+
+
+	// Use this for initialization
+	void Awake () {
+	
+		curAvailableUnit1s = maxUnit1Number;
+		curAvailableUnit2s = maxUnit2Number;
+		curAvailableUnit3s = maxUnit3Number;
+
+		lookAtPathPoint = Vector3.zero;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		//check if a unit has been spawned. If so clamp its position to the mouse and release on mouse left click.
+		if(curSpawnedUnit != null)
+		{
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			if (Physics.Raycast(ray, out hit, 100))
+			{
+				unitPosX = hit.point.x;
+				unitPosZ = hit.point.z;
+				unitFollowPos = new Vector3(unitPosX, 0.5f, unitPosZ);
+				curSpawnedUnit.transform.position = unitFollowPos;
+				lookTestSphere.transform.position = new Vector3(unitPosX, 0.01f, unitPosZ);
+
+				if(lookAtPathPoint != Vector3.zero)
+				{
+					if(Vector3.Distance(curSpawnedUnit.transform.position, lookAtPathPoint) < positioningLookDist)
+					{
+						curSpawnedUnit.transform.LookAt(new Vector3(lookAtPathPoint.x, 0.5f, lookAtPathPoint.z));
+					}
+				}
+			}
+
+			if(Input.GetMouseButtonDown(0))
+			{
+				curSpawnedUnit = null;
+				lookTestSphere.transform.position = new Vector3(0,200,0);
+				lookAtPathPoint = Vector3.zero;
+			}
+
+		}
+
+
+
+	}
+
+	//called from the Canvas button events
+	public void InstantiateDefensiveUnit_1()
+	{
+		if(curAvailableUnit1s > 0)
+		{
+			curSpawnedUnit = Instantiate(Unit1) as GameObject;
+			curAvailableUnit1s--;
+		}
+	}
+	public void InstantiateDefensiveUnit_2()
+	{
+		if(curAvailableUnit2s > 0)
+		{
+			curSpawnedUnit = Instantiate(Unit2) as GameObject;
+			curAvailableUnit2s--;
+		}
+	}
+	public void InstantiateDefensiveUnit_3()
+	{
+		if(curAvailableUnit3s > 0)
+		{
+			curSpawnedUnit = Instantiate(Unit3) as GameObject;
+			curAvailableUnit3s--;
+		}
+	}
+}
---
+++ Assets/Scripts/TurretLookAtPathTest.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TurretLookAtPathTest : MonoBehaviour {
+
+	public GameObject defenceInventoryManager;
+	DefensiveInventoryManager placementManager;
+
+	public float pointX, pointZ;
+	public Transform[] lookTestPoints;
+
+	RaycastHit hit;
+	float distToHit = 1000.0f, dist, checkTime = 0.1f, curTime;
+
+	// Use this for initialization
+	void Start () {
+	
+		curTime = 0f;
+		placementManager = defenceInventoryManager.GetComponent<DefensiveInventoryManager>();
+	}
+	
+	void Update()
+	{
+		if(transform.position != Vector3.zero)
+		{
+			if(Time.time > curTime)
+			{
+				for(int i = 0; i < lookTestPoints.Length; i++)
+				{
+					if(Physics.Raycast(transform.position, lookTestPoints[i].position - transform.position, out hit, 5.0f))
+					{
+						if(hit.collider.gameObject.CompareTag("Path"))
+						{
+							dist = Vector3.Distance(transform.position, hit.point);
+							if(dist < distToHit)
+							{
+								distToHit = dist;
+								placementManager.lookAtPathPoint = hit.point;
+							}
+						}
+					}
+				}
+
+				distToHit = 1000.0f;
+				curTime = Time.time + checkTime;
+			}
+		}
+	}
+}
