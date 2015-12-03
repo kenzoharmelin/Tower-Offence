314036cccbc95b1ab81f7a1c8bdb832f5b95bd23
Eli Whitbread <eli@whitbreadgames.com>
2015-12-03 11:09:23 +1100

    Started menu interaction scripts

---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuController : MonoBehaviour {
+
+	void GotoInstructions()
+	{
+
+	}
+
+	void GotoSettings()
+	{
+
+	}
+
+	void GotoPlay()
+	{
+
+	}
+}
---
+++ Assets/Scripts/SettingsManager.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class SettingsManager : MonoBehaviour {
+
+	public Slider brightnessSlider, volumeSlider;
+	public Text brightnessText, volumeText;
+
+
+	// Use this for initialization
+	void Start () {
+	
+		brightnessText.text = brightnessSlider.value.ToString();
+		volumeText.text = brightnessSlider.value.ToString();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void BrightnessValueChange()
+	{
+
+	}
+
+	public void VolumeValueChange()
+	{
+
+	}
+}
