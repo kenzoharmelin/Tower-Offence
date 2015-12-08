64b48106b7be39a079ada57693d8d738641e8bb0
Eli Whitbread <eli@whitbreadgames.com>
2015-12-08 18:07:12 +1100

    Fixed buttons/text resize issues. Added navigation (buttons) and started slider (settings menu) setup.

---
+++ Assets/Scripts/MenuButtonControl.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuButtonControl : MonoBehaviour {
+
+	public void GoToScene(string SceneName)
+	{
+		Application.LoadLevel(SceneName);
+	}
+
+	public void QuitGame()
+	{
+		Application.Quit();
+	}
+
+
+}
---
+++ Assets/Scripts/MenuSliderControl.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class MenuSliderControl : MonoBehaviour {
+
+	public Slider brightnessSlider, volumeSlider;
+	public Text brightnessValueText, VolumeValueText;
+	public Light[] Scenelights;
+
+	float brightnessValue, volumeValue;
+
+	// Use this for initialization
+	void Start () {
+	
+		brightnessValueText.text = brightnessSlider.value.ToString();
+		VolumeValueText.text = volumeSlider.value.ToString();
+
+	}
+	
+	public void BrightnessValueChange()
+	{
+		brightnessValueText.text = brightnessSlider.value.ToString();
+
+		foreach(Light lte in Scenelights)
+		{
+			lte.intensity = brightnessSlider.value / 10;
+		}
+	}
+
+	public void VolumeValueChange()
+	{
+		VolumeValueText.text = volumeSlider.value.ToString();
+	}
+}
