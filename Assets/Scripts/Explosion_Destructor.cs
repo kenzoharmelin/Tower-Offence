8faadf84543ace08a92ecae9e177dea8142ece37
Hugh Elleman <SpartanH97@gmail.com>
2015-12-11 13:47:22 +1100

    Pathing completed, damage monitoring effects fucntional

---
+++ Assets/Scripts/Explosion_Destructor.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Explosion_Destructor : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Invoke ("DestroySelf", 2.0f);
+	}
+
+	void DestroySelf () {
+		Destroy(gameObject);
+	}
+
+}
--- Assets/Scripts/OffenseAI.cs
+++ Assets/Scripts/OffenseAI.cs
@@ -5,17 +5,50 @@ public class OffenseAI : MonoBehaviour {
 
 	public Transform[] pathing;
 
-	public Hashtable pathInfo = new Hashtable();
+	private Hashtable pathInfo = new Hashtable();
 
-	public float speed;
+	private Object deathFX;
+
+	public float speed, curHealth, maxHealth;
+	
+	void Awake ()
+	{
+		pathing = GameObject.Find ("Path_Container").GetComponent<Pathing> ().path1;
+	}
 
 	void Start ()
 	{
 		pathInfo.Add ("path", pathing);
 		pathInfo.Add ("orienttopath", true);
-		pathInfo.Add ("speed", speed);
+		pathInfo.Add ("speed", speed * Time.deltaTime);
 		pathInfo.Add ("easetype", iTween.EaseType.linear);
 
 		iTween.MoveTo (gameObject, pathInfo);
+	}
+
+	void AdjustCurHealth (float val)
+	{
+		curHealth += val; //POSITIVE means heal, negative means damage
+		
+		if(curHealth > maxHealth) { // do I have more health than I should?
+			curHealth = maxHealth;
+		}
+		
+		if (curHealth <= 0) {
+			curHealth = 0; // DIE
+			Debug.Log (gameObject.name + " " + gameObject.GetInstanceID() + " has gone deadie-byes");
+			
+			CallDeath();
+		}
+	}
+
+	public void CallDeath()
+	{
+		int rndDFX = Random.Range (1, 13);
+		
+		deathFX = Resources.Load("Explosions/" + rndDFX);
+		
+		GameObject.Instantiate (deathFX, transform.position, Quaternion.Euler(-90f, 0f, 0f));
+		Destroy(gameObject);
 	}
 }
